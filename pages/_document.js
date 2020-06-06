043eadff1de2eb05f24b39fa58f772494532a073
Tolga <ttolgacesur@gmail.com>
2020-06-07 00:43:44 +0300

    Update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,10 @@ class MainDocument extends Document {
       <Html>
         <Head>
           <title>Tolga Cesur</title>
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2" />
+          <link rel="manifest" href="/site.webmanifest" />
         </Head>
         <body>
           <div className="main">
